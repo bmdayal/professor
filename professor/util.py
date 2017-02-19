563c1bd01f82b12c17518f3cdd536a5b01fc69b7
Brij Mohan <bmdayal@yahoo.com>
2017-02-18 20:46:04 -0600

    Changed from pymongo.Connection to pymongo.MongoClient
    pymongo no longer supports Connection, use MongoClient instead

--- professor/util.py
+++ professor/util.py
@@ -138,7 +138,7 @@ def connect_to(database):
     else:
         host = database['hostname']
         port = 27017
-    conn = pymongo.Connection(
+    conn = pymongo.MongoClient(
         host=host,
         port=port,
         network_timeout=2,
